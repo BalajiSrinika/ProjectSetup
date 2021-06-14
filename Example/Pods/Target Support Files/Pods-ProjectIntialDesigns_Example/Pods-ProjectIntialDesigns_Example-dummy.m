cb5e7a0fd95a78ab8e1c72af15c1d3a8da2796fd
Balaji <srinikabalaji@gmail.com>
2021-06-14 15:40:37 +0530

    commit pod

---
+++ Example/Pods/Target Support Files/Pods-ProjectIntialDesigns_Example/Pods-ProjectIntialDesigns_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectIntialDesigns_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectIntialDesigns_Example
+@end
---
+++ Example/Pods/Target Support Files/ProjectIntialDesigns/ProjectIntialDesigns-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ProjectIntialDesigns : NSObject
+@end
+@implementation PodsDummy_ProjectIntialDesigns
+@end
